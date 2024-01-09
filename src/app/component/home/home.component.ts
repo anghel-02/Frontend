a01a4ade10618b0eed4c1022d40e95f918106d8e
Davidecpp <cppdvd03d13m208q@studenti.unical.it>
2024-01-09 16:25:28 +0100

    login-logout

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatTabChangeEvent } from '@angular/material/tabs';
+import {AuthService} from "../../auth.service";
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,19 @@ import { MatTabChangeEvent } from '@angular/material/tabs';
   styleUrl: './home.component.css',
 })
 export class HomeComponent{
+  constructor(private authService: AuthService) {}
+  isUserLoggedIn(): boolean {
+    return this.authService.isUserLoggedIn();
+  }
+  login(): void {
+    return this.authService.login();
+  }
+  logout(): void {
+    return this.authService.logout();
+  }
 
   onTabChanged(event: MatTabChangeEvent): void {
     console.log('Scheda cambiata:', event.index);
-
   }
 
 
