10943127435adf58b91a7166f0edbeedd6613cb8
Davidecpp <cppdvd03d13m208q@studenti.unical.it>
2024-01-21 17:05:16 +0100

    img-galleria

--- src/app/component/gallery/gallery.component.ts
+++ src/app/component/gallery/gallery.component.ts
@@ -17,52 +17,45 @@ export class GalleryComponent implements OnInit , AfterViewInit{
   filteredNfts: any[] = [];
 
   constructor(private nftService: NFTService, private auth: AuthService, private router: Router, private searchService: SearchService) {}
- 
- 
+
+
     ngAfterViewInit(): void {
       this.loadOwnedNFTs();
     }
-  
+
 
     ngOnInit() {}
-  
-  loadOwnedNFTs() {
-    const username = this.auth.getUsername() ?? '';
-    this.nftService.getOwnedNFTs(username).subscribe(
-      (data: any[]) => {
-        this.ownedNFTs = data;
-        for (let el of this.ownedNFTs){
-          this.image(el.id);
-        }
 
-      },
-      (error: any) => {
-        console.error('Errore nel recupero degli NFT posseduti', error);
-      }
-    );
-
-    this.nftService.getSales().subscribe(data =>{
-      console.log('Dati ricevuti:', data);
-  }, error => {
-      console.error('Errore durante la chiamata:', error);
-      
-  })
-  }
-
-  image(id : string) {
-    
-    this.nftService.getImage(id).subscribe(
+  image(nft: any) {
+    this.nftService.getImage(nft.id).subscribe(
       (data: ArrayBuffer) => {
         const uint8Array = new Uint8Array(data);
         const byteCharacters = uint8Array.reduce((acc, byte) => acc + String.fromCharCode(byte), '');
         const imageUrl = 'data:image/png;base64,' + btoa(byteCharacters);
-        this.imageUrl = imageUrl;
+        // Assegna l'URL dell'immagine direttamente all'oggetto NFT
+        nft.imageUrl = imageUrl;
       },
       (error) => {
         console.error('Errore durante il recupero dell\'immagine', error);
       }
     );
   }
+
+  loadOwnedNFTs() {
+    const username = this.auth.getUsername() ?? '';
+    this.nftService.getOwnedNFTs(username).subscribe(
+      (data: any[]) => {
+        this.ownedNFTs = data;
+        for (let el of this.ownedNFTs){
+          this.image(el);
+        }
+      },
+      (error: any) => {
+        console.error('Errore nel recupero degli NFT posseduti', error);
+      }
+    );
+  }
+
   info(nftid: string){
     this.nftService.setnftid(nftid);
     this.router.navigate(['/nft'])
